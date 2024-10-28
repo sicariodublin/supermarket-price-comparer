61976de7bda54333225aea0016105fa78b994718
sicariodublin <fabioast47@hotmail.com>
2024-10-28 18:37:58 +0000

    feat: Privacy Policy and Therms of Services pages and css.

--- src/pages/Privacy-policy.js
+++ src/pages/Privacy-policy.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import '../styles/Privacypolicy.css';
 
 function PrivacyPolicy() {
   return (
     <div>
-      <h1>Privacy Policy</h1>
-      {/* Add your content here */}
+      <div class="privacy-container">
+        <div className="privacy-image"></div> {/* New image container */}
+        <div class="privacy-text">
+          <h1>Privacy Policy</h1>
+          <p>
+            Your privacy is important to us. It is our policy to respect your
+            privacy regarding any information we may collect from you across our
+            website.
+          </p>
+          <h2>Information we collect</h2>
+          <p>
+            We collect information you voluntarily provide to us, such as your
+            email address at sign-up, product search queries and any other
+            personal information you provide through the Addandcompare.com
+            website. We also collect non-personally identifiable information,
+            such as your IP address.
+          </p>
+          <h2>Why we collect this information</h2>
+          <p>
+            The information you provide to us enables us to fulfill your service
+            expectations. The provided information is used in various ways to
+            improve our website's interaction with the user, via newsletters or
+            personalised product displays.
+          </p>
+          <h2>Information sharing and disclosure</h2>
+          <p>
+            We do not share or disclose your personal information to any third
+            parties, except as required by law or as necessary to provide you
+            with the services you request.
+          </p>
+          <h2>Security</h2>
+          <p>
+            We take reasonable measures to protect your personal information
+            from unauthorized access, use, or disclosure. However, we cannot
+            guarantee that your information will be completely secure.
+          </p>
+          <h2>Changes to this policy</h2>
+          <p>
+            We may update this policy from time to time. We will notify you of
+            any changes by posting the new policy on our website.
+          </p>
+          <h2>Contact us</h2>
+          <p>
+            If you have any questions or concerns about our privacy policy,
+            please feel free to reach out to us, we are always happy to help!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Terms-of-service.js
+++ src/pages/Terms-of-service.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import '../styles/Termsofservice.css';
 
 function TermsOfService() {
   return (
     <div>
-      <h1>Terms Of Service</h1>
-      {/* Add your content here */}
+      <div class="terms-container">
+        <div className="terms-image"></div> {/* New image container */}
+        <div class="terms-text">
+          <h1>Terms of Service</h1>
+          <p>
+            Welcome to addandcompare.com! By accessing our website, you agree to
+            these terms of service. Please read them carefully.
+          </p>
+          <h2>Acceptance of Terms</h2>
+          <p>
+            When you use our services, you’re agreeing to our terms, so please
+            take a few minutes to read over the User Agreement below.
+          </p>
+          <h2>Changes to Terms</h2>
+          <p>
+            We may modify these terms at any time. We will notify you of any
+            changes by posting the new terms on our website.
+          </p>
+          <h2>Communication</h2>
+          <p>
+            By creating an account on our website, you agree to subscribe to
+            newsletters, marketing or promotional materials and other
+            information we may send.
+          </p>
+          <h2>Content</h2>
+          <p>
+            The content found on or through this service are the property of
+            Addandcompare.com or used with permission. You may not distribute,
+            modify, transmit, reuse, download, repost, copy, or use said
+            Content, whether in whole or in part, for commercial purposes or for
+            personal gain, without express advance written permission from us.
+          </p>
+          <h2>Accounts</h2>
+          <p>
+            When you create an account with us, you guarantee that you are above
+            the age of 18, and that the information you provide us is accurate,
+            complete, and current at all times. Inaccurate, incomplete, or
+            obsolete information may result in the immediate termination of your
+            account on our service.
+          </p>
+          <h2>Intellectual Property</h2>
+          <p>
+            The service and its original content (excluding Content provided by
+            users), features, and functionality are and will remain the
+            exclusive property of Addandcompare.com and its licensors.
+          </p>
+          <h2>Contact Us</h2>
+          <p>If you have any questions about these Terms, please contact us.</p>
+        </div>
+      </div>
     </div>
   );
 }
